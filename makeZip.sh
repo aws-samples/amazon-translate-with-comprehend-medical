d3c282e60113636d717492b017aa517cac13d98d
Weiler <mwweiler@amazon.com>
2020-02-19 20:13:36 -0800

    Updated license information

--- makeZip.sh
+++ makeZip.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: MIT-0
+
 cd ./translate-cm
 zip -r ../translate-cm.zip ./*
 cd ../translate-cm-athena
