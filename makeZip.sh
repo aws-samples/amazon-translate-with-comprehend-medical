c0e9c9a8aafe57b04778ebe4a40b3d2493ac48db
Weiler <mwweiler@amazon.com>
2020-02-19 20:28:56 -0800

    Fix newlines

--- makeZip.sh
+++ makeZip.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
-# SPDX-License-Identifier: MIT-0
-
 cd ./translate-cm
 zip -r ../translate-cm.zip ./*
 cd ../translate-cm-athena
